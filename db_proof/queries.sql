6a22832aae4bcc3f7decf61c75fcd694b4a0842f
Luimoe05 <lmoreno00528@gmail.com>
2026-04-08 20:32:25 -0400

    added a trigger to filter names to correct format

--- db_proof/queries.sql
+++ db_proof/queries.sql
@@ -16,4 +16,17 @@ SELECT * FROM grades;
 CREATE VIEW view_all_staff AS
 SELECT staff_id, first_name, last_name, staff_role FROM staff;
 
+-- #This filters the names to make sure that the first letter is capitalized and the rest are not
 
+DELIMITER //
+
+CREATE TRIGGER trg_clean_student_name
+BEFORE INSERT ON students
+FOR EACH ROW
+BEGIN 
+	SET NEW.first_name = CONCAT(UPPER(SUBSTRING(NEW.first_name, 1, 1)), LOWER(SUBSTRING(NEW.first_name, 2)));
+    SET NEW.last_name = CONCAT(UPPER(SUBSTRING(NEW.last_name, 1, 1)), LOWER(SUBSTRING(NEW.last_name, 2))); 
+END //
+
+DELIMITER ;
+    
